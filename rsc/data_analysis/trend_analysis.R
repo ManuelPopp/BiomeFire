cda778dd24b635f7492f1c70de883cf792305838
Manuel Richard Popp <manuel.popp@wsl.ch>
2025-02-21 21:01:34 +0100

    Changes to nalyses

--- rsc/data_analysis/trend_analysis.R
+++ rsc/data_analysis/trend_analysis.R
@@ -1,5 +1,7 @@
 require("dplyr")
 require("tidyr")
+require("purrr")
+require("trend")
 require("ggplot2")
 if (Sys.info()["sysname"] == "Windows") {
   dir_main <- "C:/Users/poppman/switchdrive/PhD/prj/bff"
@@ -10,18 +12,19 @@ if (Sys.info()["sysname"] == "Windows") {
 }
 
 dir_dat <- file.path(dir_main, "dat")
+dir_fig <- file.path(dir_main, "fig")
 dir_lud <- file.path(dir_dat, "lud11")
 dir_imd <- file.path(dir_lud, "intermediate_data")
 
 biome_names <- c(
-  "(Sub)trop. moist BLF",
-  "(Sub)trop. dry BLF",
-  "(Sub)trop. Coniferous",
-  "Temp. mixed",
-  "Temp. Coniferous",
+  "(Sub)tropical Moist BLF",
+  "(Sub)tropical Dry BLF",
+  "(Sub)tropical Coniferous Forest",
+  "Temperate Mixed Forest",
+  "Temperate Coniferous Forest",
   "Taiga",
   "Savanna/Grassland",
-  "Temp. Grassland",
+  "Temperate Grassland",
   "Flooded Savanna",
   "Montane Grassland",
   "Tundra",
@@ -33,7 +36,8 @@ biome_names <- c(
 files <- list.files(
   path = dir_imd, pattern = "annual_burned_area.csv",
   recursive = TRUE, full.names = TRUE
-  )
+  ) %>% # Drop Deserts and xeric shrublands
+  purrr::discard(~ grepl("Olson_biome_13", .x))
 
 df <- do.call(rbind, lapply(X = files, FUN = read.csv)) %>%
   tidyr::separate(
@@ -52,23 +56,46 @@ df <- do.call(rbind, lapply(X = files, FUN = read.csv)) %>%
 pvals <- df %>%
   dplyr::group_by(Biome, Biome_name) %>%
   dplyr::summarise(
-    p_value = summary(lm(Burn_perc ~ Year))$coefficients[2,4]
+    p_value = trend::mk.test(Burn_perc)$p.val,
+    text_x = max(year) - 3,
+    text_y = max(Burn_perc) - 0.1 * (max(Burn_perc) - min(Burn_perc))
     ) %>%
   dplyr::ungroup()
 
-ggplot2::ggplot(
+gg <- ggplot2::ggplot(
   data = df,
-  ggplot2::aes(x = Year, y = Normalised, colour = Biome_name, fill = Biome_name)
+  ggplot2::aes(x = Year, y = Burn_perc, colour = Biome_name, fill = Biome_name)
   ) +
   ggplot2::geom_point() +
   ggplot2::geom_smooth(method = "lm", se = TRUE, alpha = 0.1) +
   ggplot2::theme_bw() +
-  ggplot2::facet_wrap(.~ Biome_name, nrow = 3) +
+  ggplot2::ylab("Burned area in %") +
   ggplot2::theme(legend.position = "none") +
   ggplot2::geom_text(
     data = pvals,
     ggplot2::aes(
-      x = max(df$Year) - 3, y = 0.95,
+      x = text_x, y = text_y,
       label = paste0("p = ", signif(p_value, 3))
       )
     )
+
+dir_fig_trends <- file.path(dir_fig, "trends")
+if (!dir.exists(dir_fig_trends)) {
+  dir.create(dir_fig_trends, showWarnings = FALSE)
+}
+
+gg_wide <- gg +
+  ggplot2::facet_wrap(.~ Biome_name, nrow = 3, scales = "free_y")
+
+lapply(
+  X = file.path(dir_fig_trends, paste0("TrendsByBiomeWIDE", c(".pdf", ".svg"))),
+  FUN = ggplot2::ggsave, plot = gg_wide, width = 12, height = 8
+)
+
+gg_long <- gg +
+  ggplot2::facet_wrap(.~ Biome_name, nrow = 4, scales = "free_y")
+
+lapply(
+  X = file.path(dir_fig_trends, paste0("TrendsByBiomeLONG", c(".pdf", ".svg"))),
+  FUN = ggplot2::ggsave, plot = gg_long, width = 8, height = 10
+)
