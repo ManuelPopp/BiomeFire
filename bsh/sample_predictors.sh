626d59667fecf450f11a2b2fa746f4491204c375
ManuelPopp <geooekologie@cdpopp.de>
2025-03-08 20:06:18 +0100

    Added options, added handling of few fire pixels.

--- bsh/sample_predictors.sh
+++ bsh/sample_predictors.sh
@@ -3,13 +3,14 @@
 # Define the range of years
 start_year=2002
 end_year=2018
+biome=${1:-""}
 
 # Create an array to hold the commands
 commands=()
 
 # Generate the commands
 for year in $(seq $start_year $end_year); do
-    commands+=("nohup Rscript ../rsc/data_preparation/sample_predictors.R $year > ../log/s${year}.out 2> ../log/s${year}.err")
+    commands+=("nohup Rscript ../rsc/data_preparation/sample_predictors.R $year $biome > ../log/s${year}.out 2> ../log/s${year}.err")
 done
 
 # Use xargs to run the commands in parallel (3 at a time)
